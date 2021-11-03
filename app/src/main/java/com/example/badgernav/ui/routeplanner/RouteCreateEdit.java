38cf91f0ec4a1055ac205bcb3c1ac22d57075241
brandinooo98 <bterickson2@wisc.edu>
2021-11-03 17:28:33 -0500

    suggestion menu

--- app/src/main/java/com/example/badgernav/ui/routeplanner/RouteCreateEdit.java
+++ app/src/main/java/com/example/badgernav/ui/routeplanner/RouteCreateEdit.java
@@ -11,6 +11,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import com.example.badgernav.R;
 
@@ -32,6 +34,11 @@ public class RouteCreateEdit extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        AutoCompleteTextView view = getView().findViewById(R.id.location_input);
+        String[] locations = {"Bascom Hall", "Grainger Hall", "Ben's House"};
+        // String[] locations = getResources().getStringArray(R.array.buildings_array);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, locations);
+        view.setAdapter(adapter);
 
         mViewModel = new ViewModelProvider(this).get(RouteCreateEditViewModel.class);
     }
