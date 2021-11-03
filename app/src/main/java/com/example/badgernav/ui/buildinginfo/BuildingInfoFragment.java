5de58897b4dfa395812bd7883bd5b0b0f1d30975
benhharrison <benhharison@gmail.com>
2021-11-03 16:01:46 -0500

    Created Basic Building Info layout

--- app/src/main/java/com/example/badgernav/ui/buildinginfo/BuildingInfoFragment.java
+++ app/src/main/java/com/example/badgernav/ui/buildinginfo/BuildingInfoFragment.java
@@ -8,9 +8,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.badgernav.R;
 
@@ -25,14 +30,44 @@ public class BuildingInfoFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        Log.i("BuildingInfo","Created!");
+
         return inflater.inflate(R.layout.building_info_fragment, container, false);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        try {
+            fillDropDown();
+        }
+        catch (Exception ex) {
+            Log.e("BuildingInfo","fillDropDown Error! - " + ex.getMessage());
+        }
+
+        updateCapacity();
+
         mViewModel = new ViewModelProvider(this).get(BuildingInfoViewModel.class);
-        // TODO: Use the ViewModel
     }
+
+    private void fillDropDown() {
+        Spinner dropDown = getView().findViewById(R.id.buildingDropDownList);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.test_array,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropDown.setAdapter(adapter);
+    }
+
+    private void updateCapacity() {
+        int capacity = 10;
+        ProgressBar progressBar = (ProgressBar) getView().findViewById(R.id.capacityBar);
+        TextView capacityText = getView().findViewById(R.id.capacityText);
+
+        capacityText.setText(capacity + "%");
+        progressBar.setProgress(capacity);
+    }
+
+
 
 }
