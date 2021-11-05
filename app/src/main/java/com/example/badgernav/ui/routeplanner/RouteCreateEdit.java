b7ab1a892cc000047602367f47f1462892841fdf
brandinooo98 <bterickson2@wisc.edu>
2021-11-04 19:45:59 -0500

    Route planner setup

--- app/src/main/java/com/example/badgernav/ui/routeplanner/RouteCreateEdit.java
+++ app/src/main/java/com/example/badgernav/ui/routeplanner/RouteCreateEdit.java
@@ -2,6 +2,9 @@ package com.example.badgernav.ui.routeplanner;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -41,5 +44,38 @@ public class RouteCreateEdit extends Fragment {
         view.setAdapter(adapter);
 
         mViewModel = new ViewModelProvider(this).get(RouteCreateEditViewModel.class);
+    }
+
+    public void back(View view){
+        Intent intent = new Intent(getContext(), RoutePlannerFragment.class);
+        startActivity(intent);
+    }
+
+    public void delete(View view){
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(getContext());
+        builder1.setTitle("Are you sure you want to delete?");
+        builder1.setMessage("Deletion will remove the stop from the route");
+        builder1.setCancelable(true);
+
+        builder1.setPositiveButton(
+                "Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent = new Intent(getContext(), RoutePlannerFragment.class);
+                        startActivity(intent);
+                        dialog.cancel();
+                    }
+                });
+
+        builder1.setNegativeButton(
+                "No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
     }
 }
--- app/src/main/java/com/example/badgernav/ui/routeplanner/RoutePlannerFragment.java
+++ app/src/main/java/com/example/badgernav/ui/routeplanner/RoutePlannerFragment.java
@@ -2,6 +2,7 @@ package com.example.badgernav.ui.routeplanner;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.badgernav.R;
 
@@ -31,8 +35,28 @@ public class RoutePlannerFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        // Creates the spinner
+        Spinner spinner = (Spinner) getView().findViewById(R.id.methodSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.methods_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
         mViewModel = new ViewModelProvider(this).get(RoutePlannerViewModel.class);
         // TODO: Use the ViewModel
     }
+
+    // TODO: decide what to do when a method is selected
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    public void onClick(View view){
+        Intent intent = new Intent(getContext(), RouteCreateEdit.class);
+        startActivity(intent);
+    }
+
 
 }
