02523dca79db41e8c76009e6b566a9e8f59b7986
Master-Gecko <zopf@wisc.edu>
2021-12-13 10:28:43 -0600

    mMarkers isn't being updated

--- app/src/main/java/com/example/badgernav/ui/routeplanner/RoutePlannerFragment.java
+++ app/src/main/java/com/example/badgernav/ui/routeplanner/RoutePlannerFragment.java
@@ -1,5 +1,7 @@
 package com.example.badgernav.ui.routeplanner;
 
+import androidx.annotation.RequiresApi;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
@@ -11,6 +13,7 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -20,7 +23,10 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,6 +40,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.badgernav.models.Event;
+import com.example.badgernav.models.PolylineData;
 import com.example.badgernav.util.EventDBHelper;
 import com.example.badgernav.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -47,13 +54,23 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.DirectionsApiRequest;
 import com.google.maps.GeoApiContext;
+import com.google.maps.PendingResult;
+import com.google.maps.internal.PolylineEncoding;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.TravelMode;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
 @SuppressLint("LongLogTag")
 public class RoutePlannerFragment extends Fragment implements OnMapReadyCallback,
@@ -71,9 +88,8 @@ public class RoutePlannerFragment extends Fragment implements OnMapReadyCallback
     private RecyclerView recyclerView;
     private GoogleMap mGoogleMap;
     private MapView mMapView;
-    private FusedLocationProviderClient mFusedLocationProviderClient;
-    private RoutePlannerViewModel mViewModel;
 
+    private ArrayList<PolylineData> mPolyLinesData = new ArrayList<>();
     private ArrayList<Marker> mMarkers = new ArrayList<>();
     private GeoApiContext mGeoApiContext = null;
 
@@ -86,6 +102,72 @@ public class RoutePlannerFragment extends Fragment implements OnMapReadyCallback
         mMapView = (MapView) view.findViewById(R.id.mapView);
         initGoogleMap(savedInstanceState);
         return view;
+    }
+
+    private void calculateDirections(Marker org, Marker dst) {
+        Log.d(TAG, "calculateDirections: calculating directions.");
+
+        com.google.maps.model.LatLng destination = new com.google.maps.model.LatLng(
+                dst.getPosition().latitude,
+                dst.getPosition().longitude
+        );
+        com.google.maps.model.LatLng origin = new com.google.maps.model.LatLng(
+                org.getPosition().latitude,
+                org.getPosition().longitude
+        );
+        DirectionsApiRequest directions = new DirectionsApiRequest(mGeoApiContext);
+        directions.origin(origin);
+        directions.mode(TravelMode.WALKING);
+        Log.d(TAG, "calculateDirections: destination: " + destination.toString());
+        directions.destination(destination).setCallback(new PendingResult.Callback<DirectionsResult>() {
+            @Override
+            public void onResult(DirectionsResult result) {
+                Log.d(TAG, "calculateDirections: routes: " + result.routes[0].toString());
+                Log.d(TAG, "calculateDirections: duration: " + result.routes[0].legs[0].duration);
+                Log.d(TAG, "calculateDirections: distance: " + result.routes[0].legs[0].distance);
+                Log.d(TAG, "calculateDirections: geocodedWayPoints: " + result.geocodedWaypoints[0].toString());
+
+                addPolylinesToMap(result);
+            }
+
+            @Override
+            public void onFailure(Throwable e) {
+                Log.e(TAG, "calculateDirections: Failed to get directions: " + e.getMessage());
+
+            }
+        });
+    }
+
+    private void addPolylinesToMap(final DirectionsResult result) {
+        new Handler(Looper.getMainLooper()).post(() -> {
+            Log.d(TAG, "run: result routes: " + result.routes.length);
+            if (mPolyLinesData.size() > 0) {
+                for (PolylineData polylineData : mPolyLinesData) {
+                    polylineData.getPolyline().remove();
+                }
+                mPolyLinesData.clear();
+                mPolyLinesData = new ArrayList<>();
+            }
+
+            for (DirectionsRoute route : result.routes) {
+                Log.d(TAG, "run: leg: " + route.legs[0].toString());
+                List<com.google.maps.model.LatLng> decodedPath = PolylineEncoding.decode(route.overviewPolyline.getEncodedPath());
+
+                List<LatLng> newDecodedPath = new ArrayList<>();
+
+                // This loops through all the LatLng coordinates of ONE polyline.
+                for (com.google.maps.model.LatLng latLng : decodedPath) {
+
+                    newDecodedPath.add(new LatLng(
+                            latLng.lat,
+                            latLng.lng
+                    ));
+                }
+                Polyline polyline = mGoogleMap.addPolyline(new PolylineOptions().addAll(newDecodedPath));
+                polyline.setColor(ContextCompat.getColor(getActivity(), R.color.blue));
+                mPolyLinesData.add(new PolylineData(polyline, route.legs[0]));
+            }
+        });
     }
 
     private void initGoogleMap(Bundle savedInstanceState) {
@@ -122,15 +204,11 @@ public class RoutePlannerFragment extends Fragment implements OnMapReadyCallback
         recyclerView = getView().findViewById(R.id.recyclerView);
         setAdapter();
 
-        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
         // Creates the spinner
         Spinner spinner = (Spinner) getView().findViewById(R.id.methodSpinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.methods_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-
-        mViewModel = new ViewModelProvider(this).get(RoutePlannerViewModel.class);
-
     }
 
     private void setAdapter() {
@@ -184,6 +262,7 @@ public class RoutePlannerFragment extends Fragment implements OnMapReadyCallback
         mMapView.onStop();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onMapReady(GoogleMap map) {
         try {
@@ -216,25 +295,56 @@ public class RoutePlannerFragment extends Fragment implements OnMapReadyCallback
                 mMarkers.clear();
                 mMarkers = new ArrayList<>();
             }
+
+            if (mPolyLinesData.size() > 0) {
+                mPolyLinesData.clear();
+                mPolyLinesData = new ArrayList<>();
+            }
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     private void addMapMarkers() {
+        ArrayList<String> eBuildings = new ArrayList<>();
+        EventDBHelper dbHelper = new EventDBHelper(sqLiteDatabase);
+        dbHelper.createTable();
+        ArrayList<Event> eventList = dbHelper.getEvents();
+
         if (mGoogleMap != null) {
             resetMap();
 
-            ArrayList<String> eBuildings = new ArrayList<>();
-            EventDBHelper dbHelper = new EventDBHelper(sqLiteDatabase);
-            dbHelper.createTable();
-            ArrayList<Event> eventList = dbHelper.getEvents();
             for (Event e : eventList) {
                 eBuildings.add(e.getBuilding());
                 getPlaceInfo(e, mGoogleMap);
+                Log.d(TAG, "addMapMarkers: " + mMarkers.size() + " markers");
             }
 
             Log.i(TAG, String.format("addMapMarkers: Event Buildings %s", eBuildings));
             mGoogleMap.setOnInfoWindowClickListener(this);
             setMapStartingView();
+
+            if (mMarkers.size() > 1) {
+                ArrayList<Pair> pairs = new ArrayList<>();
+                IntStream.range(1, eventList.size())
+                        .mapToObj(i -> new Pair(eventList.get(i-1), eventList.get(i)))
+                        .forEach(pairs::add);
+                for (Pair p : pairs) {
+                     Marker org = null;
+                     Marker dst = null;
+                     for (Marker m : mMarkers) {
+                         if (m.getTag().equals(p.first)) {
+                             org = m;
+                         } else if (m.getTag().equals(p.second)) {
+                             dst = m;
+                         }
+                     }
+                     if (org != null && dst != null) {
+                         calculateDirections(org, dst);
+                     } else {
+                         Log.e(TAG, "addMapMarkers: Unable to find org and dst for polyline");
+                     }
+                }
+            }
         }
     }
 
@@ -297,6 +407,7 @@ public class RoutePlannerFragment extends Fragment implements OnMapReadyCallback
                                 .title(name));
                         marker.setTag(e);
                         mMarkers.add(marker);
+                        Log.d(TAG, "getGeocode: " + mMarkers.size() + " markers");
                     } catch (JSONException e1) {
                         Log.e(TAG, "getGeocode: Geocode API call fail: " + e1.toString());
                     }
@@ -325,6 +436,7 @@ public class RoutePlannerFragment extends Fragment implements OnMapReadyCallback
         mMapView.onLowMemory();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onInfoWindowClick(@NonNull Marker marker) {
         final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
