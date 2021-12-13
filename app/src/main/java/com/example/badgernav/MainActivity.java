6a3e3927845217f3c34f697422862871128a3216
benhharrison <benhharison@gmail.com>
2021-12-13 02:11:10 -0600

    Integrated Calendar screen into rest of app; fixed color scheme

--- app/src/main/java/com/example/badgernav/MainActivity.java
+++ app/src/main/java/com/example/badgernav/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.badgernav;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (getSharedPreferences("AppSettingPrefs", 0).getBoolean("NightMode", false)) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+        else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
 
         Intent intent = new Intent(this, NavMenuActivity.class);
         startActivity(intent);
--- app/src/main/java/com/example/badgernav/ui/calendar/MainCalendarActivity.java
+++ app/src/main/java/com/example/badgernav/ui/calendar/MainCalendarActivity.java
@@ -33,7 +33,7 @@ public class MainCalendarActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_calendar);
 
         data = new Data(MainCalendarActivity.this);
 
